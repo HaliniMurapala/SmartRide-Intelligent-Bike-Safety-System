17a9df9093182c88fb6f8f8d9a2366d3c5e59a7b
MURAPALA HALINI <131904362+HaliniMurapala@users.noreply.github.com>
2026-05-25 12:23:07 +0530

    Add Bluetooth communication to helmet_node

---
+++ Helmet_Node/helmet_node.ino
@@ -0,0 +1,35 @@
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
+// Replace with your server MAC
+uint8_t serverAddress[] = {0xC0,0xCD,0xD6,0x84,0x9D,0x4A};
+#define IR_PIN 4
+#define MQ3_PIN 34
+void setup() {
+  pinMode(IR_PIN, INPUT);
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_Client", true);
+  delay(10000);
+  Serial.println("Connecting...");
+  if (SerialBT.connect(serverAddress)) {
+    Serial.println("Connected!");
+  } else {
+    Serial.println("Failed!");
+  }
+}
+void loop() {
+  int irValue = digitalRead(IR_PIN);
+  int alcoholValue = analogRead(MQ3_PIN);
+  // Format:
+  // IR:0,MQ3:1234
+  String Data = "IR:" + String(irValue) +
+                ",MQ3:" + String(alcoholValue);
+  if (SerialBT.connected()) {
+    SerialBT.println("Hello Server");
+    SerialBT.println(Data);
+    Serial.println(Data);
+    Serial.println("Sent");
+  } else {
+    Serial.println("Disconnected");
+  }
+  delay(2000);
+}
